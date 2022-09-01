d53e99fc043b18258987a386a1a6b8e68de0ecea
AlbertoLanchas <albertolsn7@gmail.com>
2022-09-01 11:45:38 +0200

    chart js implemented
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/Pages/CoinPage.jsx
+++ src/Pages/CoinPage.jsx
@@ -4,8 +4,9 @@ import { useParams } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
 import { SingleCoin } from "../config/api";
 import CoinInfo from "../components/CoinInfo";
-import { Typography } from "@mui/material";
+import { LinearProgress, Typography } from "@mui/material";
 import parse from "html-react-parser";
+import { numberWithCommas } from "../components/CoinsTable";
 
 const CoinPage = () => {
   const { id } = useParams();
@@ -24,6 +25,8 @@ const CoinPage = () => {
     fetchCoin();
   }, [currency]);
 
+  if (!coin) return <LinearProgress style={{ backgroundColor: "violet" }} />;
+
   return (
     <div
       style={{
@@ -40,6 +43,7 @@ const CoinPage = () => {
           alignItems: "center",
           marginTop: 25,
           borderRight: "2px solid grey",
+          padding: 30,
         }}
       >
         <img
@@ -62,8 +66,8 @@ const CoinPage = () => {
           variant="subtitle1"
           style={{
             width: "100%",
-            fontFamily: "Montserrat",
-            padding: 25,
+            fontFamily: "Arial",
+            // padding: 25,
             paddingBottom: 15,
             paddingTop: 0,
             textAlign: "justify",
@@ -71,6 +75,101 @@ const CoinPage = () => {
         >
           {parse(`${coin?.description.en.split(". ")[0]}`)}.
         </Typography>
+
+        <div
+          type={{
+            alignSelf: "start",
+            padding: 25,
+            paddingTop: 10,
+            width: "100%",
+            // [theme.breakpoints.down("md")]: {
+            //   display: "flex",
+            //   justifyContent: "space-around",
+            // },
+            // [theme.breakpoints.down("sm")]: {
+            //   flexDirection: "column",
+            //   alignItems: "center",
+            // },
+            // [theme.breakpoints.down("xs")]: {
+            //   alignItems: "start",
+            // },
+          }}
+        >
+          <span style={{ display: "flex" }}>
+            <Typography
+              variant="h5"
+              style={{
+                fontWeight: "bold",
+                marginBottom: 20,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {numberWithCommas(coin?.market_cap_rank)}
+            </Typography>
+          </span>
+
+          <span style={{ display: "flex" }}>
+            <Typography
+              variant="h5"
+              style={{
+                fontWeight: "bold",
+                marginBottom: 20,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+
+          <span style={{ display: "flex" }}>
+            <Typography
+              variant="h5"
+              style={{
+                fontWeight: "bold",
+                marginBottom: 20,
+                fontFamily: "Montserrat",
+              }}
+            >
+              Market Cap:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {symbol}{" "}
+              {numberWithCommas(
+                coin?.market_data.market_cap[currency.toLowerCase()]
+                  .toString()
+                  .slice(0, -6)
+              )}
+              M
+            </Typography>
+          </span>
+        </div>
       </div>
       <CoinInfo coin={coin} />
     </div>
--- src/components/CoinInfo.jsx
+++ src/components/CoinInfo.jsx
@@ -1,7 +1,121 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
+import { HistoricalChart } from "../config/api";
+import { createTheme, ThemeProvider, CircularProgress } from "@mui/material";
+import { Line } from "react-chartjs-2";
+import { Chart } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  Title,
+  LineElement,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from "chart.js";
+ChartJS.register(
+  Title,
 
-const CoinInfo = () => {
-  return <div>CoinInfo</div>;
+  LineElement,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement
+);
+
+const CoinInfo = ({ coin }) => {
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = CryptoState();
+  const [flag, setflag] = useState(false);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setflag(true);
+    setHistoricData(data.prices);
+  };
+  console.log(
+    "data",
+    historicData?.map((coin) => coin[0])
+  );
+  // console.log("data", historicData[0]);
+
+  useEffect(() => {
+    fetchHistoricData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency, days]);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div
+        style={{
+          width: "75%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 25,
+          padding: 40,
+          // [theme.breakpoints.down("md")]: {
+          //   width: "100%",
+          //   marginTop: 0,
+          //   padding: 20,
+          //   paddingTop: 0,
+          // },
+        }}
+      >
+        {!historicData | (flag === false) ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicData?.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historicData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Days ) in ${currency}`,
+                    borderColor: "#EEBC1D",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+            <div>{chartDays}</div>
+          </>
+        )}
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default CoinInfo;
